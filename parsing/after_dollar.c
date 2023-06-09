37ba95c3dc119cff709e4b958ab168c265c3f0ed
Hassan <cybertrack500@gmail.com>
2023-06-09 22:05:50 +0100

    lupdate

--- parsing/after_dollar.c
+++ parsing/after_dollar.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   after_dollar.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hben-mes <hben-mes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/09 17:56:38 by hben-mes          #+#    #+#             */
-/*   Updated: 2023/06/09 21:58:07 by hben-mes         ###   ########.fr       */
+/*   Created: 2023/06/09 22:03:15 by hben-mes          #+#    #+#             */
+/*   Updated: 2023/06/09 22:03:17 by hben-mes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
-
-
-
-
-
-
-
-
-
-#endif
