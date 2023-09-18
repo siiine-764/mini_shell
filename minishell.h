a235e35beb024d286d64dbf266bc2124ce7d68c9
mayache- <mayache-@student.1337.ma>
2023-09-18 19:01:26 +0100

    Committing changes to minishell.h

--- minishell.h
+++ minishell.h
@@ -6,10 +6,7 @@
 /*   By: mayache- <mayache-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 17:56:38 by hben-mes          #+#    #+#             */
-/*   Updated: 2023/06/13 02:41:46 by mayache-         ###   ########.fr       */
-/*   Updated: 2023/06/09 22:08:38 by hben-mes         ###   ########.fr       */
-/*   Updated: 2023/06/09 20:17:36 by mayache-         ###   ########.fr       */
-/*   Updated: 2023/06/09 21:58:07 by hben-mes         ###   ########.fr       */
+/*   Updated: 2023/09/18 18:05:38 by mayache-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +21,12 @@
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+#include <fcntl.h>
 #include <limits.h>
 
 char **g_env;
 
+#define CMD 1;
 struct Node {
   char      *val;
   char      *key;
@@ -48,10 +47,16 @@ typedef struct s_add_env
     char            **new_env;
 }	t_add_env;
 
+// typedef struct s_ast_node
+// {
+//   enum e_node_type	type;
+//   t_union				*content;
+// }	t_ast_node;
 
 /// functions builtins ///
 void		pwd(void);
-void		cd(char **env, t_env *e);
+// void		cd(char **env, t_env *e);
+void  cd(char **env, struct Node* head);
 void		un_set(struct Node* head, char *add_key);
 void		ft_ex_port(struct Node* head, char *add_key, char *add_val);
 void		echo(char **arr);
@@ -62,6 +67,7 @@ void		ex_it(void);
 struct Node	*createNode(char  *val, char  *key);
 void		insertNode(struct Node** head, char  *val, char  *key);
 void		displayList(struct Node* head);
+void    displayList_export(struct Node* head);
 void		deleteNode(struct Node** head_ref, const char* key);
 
 /// functions env ///
@@ -70,13 +76,16 @@ int			str_len(char *env);
 char		*ft_str_dup(char *s1);
 char		*str_join(char *s1, char *s2);
 char		**ft_arr_dup(char **origin);
-char		*get_key(char *str, t_env *e);
-char		*get_value(char *str, t_env *e);
+char		*get_key(char *str);
+char		*get_value(char *str);
 void		get_env(char **env, t_env   *e);
+
 /// end functions env ///
 
-
-
+void exect(void);
+void	create_env(char **env, struct Node** head);
+int	ft_strcmp(char *s1, char *s2);
+int	ft_isalpha(int c);
 
 
 
