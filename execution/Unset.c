1b68a0aa3a63509e8e1ed60ed6da6be750da7c09
Hassan <cybertrack500@gmail.com>
2023-10-03 23:55:40 +0100

    never give up

---
+++ execution/Unset.c
@@ -0,0 +1,81 @@
+#include "../minishell.h"
+
+t_env	*delete_head(char **command, t_env **env_list, char *del)
+{
+	t_env	*temp;
+
+	if (ft_strcmp(command[0], del) == 0)
+	{
+		temp = *env_list;
+		*env_list = (*env_list)->nxt;
+		free(temp->ctt);
+		free(temp);
+		free_2d_array(command);
+		set_exit_code(EXIT_SUCCESS);
+		return (*env_list);
+	}
+	return (NULL);
+}
+
+void	delete_body(t_info *data)
+{
+	data->temp = data->sec;
+	free_2d_array(data->c);
+	data->fst->nxt = data->sec->nxt;
+	free(data->temp->ctt);
+	free(data->temp);
+}
+
+void	ft_unset(t_env **env_list, char *del)
+{
+	t_env	*top;
+	t_info	data;
+
+	if (!*env_list)
+		return ;
+	data.fst = *env_list;
+	data.c = ft_split((*env_list)->ctt, '=');
+	if (data.c[0] == NULL)
+		return ;
+	top = delete_head(env_list, data.c, del);
+	if (top != NULL)
+		return ;
+	if (data.fst == NULL)
+		data.sec = data.fst->nxt;
+	else
+	{
+		ft_putendl_fd("IAM HERE LOL", 2);
+		return ;
+	}
+	while (data.sec)
+	{
+		free_2d_array(data.c);
+		data.c = ft_split(data.sec->ctt, '=');
+		if (ft_strcmp(data.c[0], del) == 0)
+		{
+			delete_body(&data);
+			return ;
+		}
+		data.fst = data.sec;
+		data.sec = data.sec->nxt;
+	}
+	free_2d_array(data.c);
+	set_exit_code(EXIT_SUCCESS);
+}
+
+bool	run_unset(t_data *data, t_comm *comm)
+{
+	int	i;
+
+	if (!ft_strcmp(comm->flags[0], "unset"))
+	{
+		i = 0;
+		while (comm->flags[++i])
+		{
+			ft_unset(&data->env_list, comm->flags[i]);
+			ft_unset(&data->pub_list, comm->flags[i]);
+		}
+		return (true);
+	}
+	return (false);
+}
---
+++ execution/export.c
@@ -0,0 +1,101 @@
+#include "../minishell.h"
+
+void	ft_export(t_comm *comm, t_env *env, char *var)
+{
+	int	fd;
+
+	fd = open_files(*comm->redirection).fd_out;
+	if (fd == -1)
+	{
+		set_exit_code(1);
+		return ;
+	}
+	if (var == NULL)
+	{
+		sort_list(&env);
+		while (env)
+		{
+			ft_putstr_fd("DEC -x\t", fd);
+			ft_putendl_fd(env->ctt, fd);
+			env = env->nxt;
+		}
+	}
+	else
+	{
+		ft_lstadd_front(&env, ft_lstnew(var));
+		sort_list(&env);
+	}
+}
+
+void	show_export_list(t_comm *comm, t_data data, t_frame frame)
+{
+	t_frame	fm;
+
+	fm = open_files(*comm->redirection);
+	if (fm.fd_in == -1 || fm.fd_out == -1)
+	{
+		set_exit_code(1);	
+		return ;
+	}
+	while (data.pub_list && data.pub_list->ctt != NULL)
+	{
+		if (fm.fd_out == STDOUT_FILENO)
+		{
+			ft_putstr_fd("DEC -x  ", frame.fd_out);
+			ft_putendl_fd(data.pub_list->ctt, frame.fd_out);
+		}
+		else
+		{
+			ft_putstr_fd("DEC -x  ", fm.fd_out);
+			ft_putendl_fd(data.pub_list->ctt, fm.fd_out);
+		}
+		data.pub_list = data.pub_list->nxt;
+	}
+}
+
+void	add_unexisted_variable(t_data *data, t_comm*comm,
+		char **s, int i)
+{
+	ft_unset(&data->env_list, s[0]);
+	ft_unset(&data->pub_list, s[0]);
+	ft_lstadd_front(&(data)->env_list, ft_lstnew(ft_strdup(comm->flags[i])));
+	ft_lstadd_front(&(data)->pub_list,
+		ft_lstnew(ft_strdup(comm->flags[i])));
+	sort_list(&data->pub_list);
+}
+
+void	add_existed_variable(t_data *data, t_comm *comm, 
+		char **s, int i)
+{
+	ft_unset(&data->env_list, s[0]);
+	ft_unset(&data->pub_list, s[0]);
+	ft_lstadd_front(&(data)->env_list, ft_lstnew(ft_strdup(comm->flags[i])));
+	ft_lstadd_front(&(data)->pub_list,
+		ft_lstnew(ft_strdup(comm->flags[i])));
+	sort_list(&data->pub_list);
+}
+
+bool	run_export(t_comm *comm, t_data *data, t_frame frame)
+{
+	int			flag;
+	int			j;
+
+	flag = 0;
+	j = 0;
+	if (ft_strcmp(comm->flags[0], "exp")
+		&& ft_strcmp(comm->flags[0], "EXP"))
+		return (false);
+	if (comm->flags[1] == NULL)
+		show_export_list(comm, *data, frame);
+	else
+	{
+		while (comm->flags[++i])
+		{
+			if (is_properly_named(comm->flags[i]))
+				add_properly_named_word(comm, data, j);
+			else
+				show_export_error(&flag, j, comm);
+		}
+	}
+	return (true);
+}
